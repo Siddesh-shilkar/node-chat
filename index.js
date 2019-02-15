3b406f4161cb719b5a3c014526f5ca100eec0e30
Siddesh Shilkar <Siddesh.Shilkar@harbingergroup.com>
2019-02-15 18:07:03 +0530

    added socket io

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const path = require('path');
-const express = require('express'); 
+const http = require('http');
+const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, './public');
 const app = express();
 const PORT = 3000 || process.env.PORT;
 
+let server = http.createServer(app);
+let io = socketIO(server);
+
 app.use(express.static(publicPath));
 
-app.listen(PORT, () => {
+io.on('connection', (socket) => {
+    console.log('New User Connected.');
+
+    socket.on('disconnect', (socket) => {
+        console.log('User was disconnected'); 
+    })
+});
+
+server.listen(PORT, () => {
     console.log(`Connected to port: ${PORT}`);
 })
