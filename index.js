49c151a8c5cf14e88febc4dc0dd457ff6d28a04e
Siddesh Shilkar <Siddesh.Shilkar@harbingergroup.com>
2019-02-18 12:13:29 +0530

    code to the send messages to both end

--- index.js
+++ index.js
@@ -14,15 +14,13 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
     console.log('New User Connected.');
-
-    socket.emit('newEmail', {
-        from: "shilkarsiddesh@gmail.com",
-        text: "Hello",
-        createdAt: 123
-    });
-
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        })
     });
 
     socket.on('disconnect', () => {
