3f7cf543dc4d332611c8a4fb7698ed85e33ed22e
Siddesh Shilkar <Siddesh.Shilkar@harbingergroup.com>
2019-02-18 12:35:21 +0530

    new user alert

--- index.js
+++ index.js
@@ -14,13 +14,30 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
     console.log('New User Connected.');
+
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text:'Welcome to the chat app'
+    });
+
+    socket.broadcast.emit('newMessage',{
+        from: 'Admin',
+        text: 'New User joined',
+        createdAt: new Date().getTime()
+    })
+
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
-        io.emit('newMessage', {
+        // io.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime()
+        // })
+        socket.broadcast.emit('newMesssage', {
             from: message.from,
             text: message.text,
             createdAt: new Date().getTime()
-        })
+        });
     });
 
     socket.on('disconnect', () => {
