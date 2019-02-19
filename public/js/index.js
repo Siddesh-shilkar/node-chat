ae2905985a60f4b5d2aee7dd90f022e21c23405f
Siddesh Shilkar <Siddesh.Shilkar@harbingergroup.com>
2019-02-19 10:33:13 +0530

    added time formats

--- public/js/index.js
+++ public/js/index.js
@@ -9,29 +9,31 @@ socket.on('disconnect', function() {
 });
 
 socket.on('newMessage', function(message) {
-    console.log('newMessage', message);
+    var formattedTime = moment(message.createdAt).format('h:mm a');
+
     var li = $('<li></li>')
-    li.text(`${message.from}: ${message.text}`);
+    li.text(`${message.from} ${formattedTime}: ${message.text}`);
 
     $('#messages').append(li);
 });
 
 socket.on('newLocationMessage', function(message){
+    var formattedTime = moment(message.createdAt).format('h:mm a');
     var li = $('<li></li>');
     var a = $('<a target="_blank">My Current Location</a>');
 
-    li.text(`${message.from}: `);
+    li.text(`${message.from} ${formattedTime}: `);
     a.attr('href', message.url);
     li.append(a);
     $('#messages').append(li);
 });
 
-socket.emit('createMessage', {
-    from: 'Jaja',
-    text: 'Hi'
-}, function(){
-    console.log('Got it');
-});
+// socket.emit('createMessage', {
+//     from: 'Jaja',
+//     text: 'Hi'
+// }, function(){
+//     console.log('Got it');
+// });
 
 $('#message-form').on('submit', function(e){
     e.preventDefault();
